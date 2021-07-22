0c8b6b9053f97ae63f2c9e5768d829d07318f10d
Steven Murray <44906675+StevenWMurray@users.noreply.github.com>
2021-07-22 17:46:14 -0500

    Add tag manager API CLI interface
    This commit adds a new CLI for invoking the Google Tag Manager API. This
    is split into 2 files. The 'tagmanager.py' file is the actual CLI tool.
    It reads its argument data in from a JSON file, which is produced by
    parsing the Tag Manager API discovery document. The
    'utilities/parse-tagmanager-discovery.jq' file parses the discovery
    document into the required format.

---
+++ tagmanager.py
@@ -0,0 +1,177 @@
+#!/usr/bin/env python
+"""Command line tool for making requests to the GA Management API"""
+
+import json
+import argparse
+import time
+import random
+import sys
+from functools import reduce
+from pathlib import Path
+from typing import Literal
+
+from googleapiclient.errors import HttpError
+from google_auth import Services
+
+service=Services.from_auth_context("GoogleAds").tagmanager_service
+arg_data_path=Path("utilities/tagmanager-args.json")
+PathArgType = Literal["path", "parent", None]
+
+get_invoke = lambda obj, key: getattr(obj, key)()
+
+class ApiPath:
+    """API Path aware argument & client library handling"""
+    def __init__(self, service: Services, lib_path: list[str], endpoint: str,
+                 path_var: PathArgType) -> None:
+        """Setup the correct client library object & path variable parser"""
+        self.lib_path = lib_path
+        self.path_var = path_var
+        self.endpoint = endpoint
+        self.create_library_function(service)
+        self.prepare_path_parser()
+
+    def create_library_function(self, service: Services) -> None:
+        """Create library function handler"""
+        get_invoke = lambda obj, key: getattr(obj, key)()
+        self.library_function = getattr(
+            reduce(get_invoke, self.lib_path, service), self.endpoint)
+
+    def prepare_path_parser(self):
+        """Build the "parse path" function
+
+        Need to:
+            - Decide whether to trim the libpath
+            - Zip the libPath objects w/ parsed args
+        """
+        def parse_path_args(cli_args: argparse.Namespace) -> None:
+            """Compile separate CLI arguments into a single path arg"""
+            mangleName = lambda instr: instr[:-1] + "Id"
+            path_args = [getattr(cli_args, mangleName(arg)) for arg in lib_path]
+            for arg in lib_path:
+                delattr(cli_args, mangleName(arg))
+            path = "/".join("/".join(e) for e in zip(lib_path, path_args))
+            setattr(cli_args, self.path_var, path)
+
+        if self.path_var is None:
+            self.parse_path_args = lambda x: None
+            return
+        if self.path_var == "path":
+            lib_path = self.lib_path
+        elif self.path_var == "parent":
+            lib_path = self.lib_path[:-1]
+        self.parse_path_args = parse_path_args
+
+    def do_special_arg_handling(self, cli_args):
+        """Subparser-specific handling of parsed CLI arguments
+
+        Currently, only includes 'folders > move_entities_to_folder' commands.
+        This splits the 3 "entityId" fields, input as comma-separated strings,
+        into lists, as expected by the client library.
+        """
+        if self.endpoint == "move_entities_to_folder":
+            for arg in ["tagId", "triggerId", "variableId"]:
+                argval = getattr(cli_args, arg)
+                if argval is not None:
+                    setattr(cli_args, arg, argval.split(","))
+
+
+type_map = {
+    "string": str,
+    "integer": int,
+    "boolean": bool,
+    "FILE": argparse.FileType('r')
+}
+
+def parse_arg_data(data_path: Path) -> dict:
+    """Serialize arg data JSON into code
+
+    In addition to loading the json, this substitutes a couple values in the
+    arg data with functions.
+    """
+    arg_data = json.loads(data_path.read_text())
+    for entity in arg_data:
+        for endpoint in entity["endpoints"]:
+            endpoint["path_handler"] = ApiPath(
+                service,
+                entity["libraryPath"],
+                endpoint["name"],
+                endpoint["pathVar"])
+            for arg in endpoint["args"]:
+                arg["data"]["type"] = type_map[arg["data"]["type"]]
+                if arg["name"] == "body":
+                    arg["data"]["nargs"] = "?"
+                    arg["data"]["default"] = sys.stdout
+    return arg_data
+
+
+def add_entity_type_parser(parser, entity_type: dict) -> None:
+    """Simplified interface to add entity type parsers to argparser"""
+    entity_parser = parser.add_parser(entity_type['name'], help=entity_type['help'])
+    endpoints = entity_type['endpoints']
+    subparser = entity_parser.add_subparsers(
+        description="Declare which endpoints to call",
+        required=True)
+
+    for endpoint in endpoints:
+        add_endpoint_parser(subparser, endpoint)
+
+
+def add_endpoint_parser(parser, endpoint: dict) -> None:
+    """Simplified interface to add endpoint parsers to argparser"""
+    endpoint_parser = parser.add_parser(endpoint['name'], help=endpoint['help'])
+    endpoint_parser.set_defaults(path_handler=endpoint['path_handler'])
+    for arg in endpoint['args']:
+        endpoint_parser.add_argument(arg['name'], **arg.get('data', {}))
+
+
+def sendRequest(request):
+    """Make API requests with exponential backoff"""
+    retryable_errors = [
+        'userRateLimitExceeded',
+        'quotedExceeded',
+        'internalServerError',
+        'backendError'
+    ]
+
+    while True:
+        try:
+            return request.execute()
+
+        except HttpError as error:
+            if error.resp.reason in retryable_errors and n <= 5:
+                time.sleep((2 ** n) + random.random())
+                n += 1
+            else:
+                raise error
+
+
+def init_parsers(parser: argparse.ArgumentParser) -> None:
+    """Add an argument parser for each supported entity type"""
+    subparser = parser.add_subparsers(
+        description="Declare which entity type to operate on",
+        required=True)
+
+    for entity_type in parse_arg_data(arg_data_path):
+        add_entity_type_parser(subparser, entity_type)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Create requests against the GA Management API")
+
+    init_parsers(parser)
+    cmd_args = parser.parse_args()
+    path_handler = cmd_args.path_handler
+    path_handler.parse_path_args(cmd_args)
+    breakpoint()
+    path_handler.do_special_arg_handling(cmd_args)
+    delattr(cmd_args, 'path_handler')
+
+    if hasattr(cmd_args, 'body'):
+        cmd_args.body = json.load(cmd_args.body)
+    request = path_handler.library_function(**vars(cmd_args))
+    response = sendRequest(request)
+    if hasattr(response, "decode"):
+        response = json.loads(response)
+    json.dump(response, sys.stdout)
+    print()
---
+++ utilities/parse-tagmanager-discovery.jq
@@ -0,0 +1,97 @@
+#!/usr/bin/env jq
+
+{
+  Template: "CustomTemplate",
+  VersionHeader: "ContainerVersionHeader",
+  Version: "ContainerVersion"
+} as $customSchemaMapping |
+.schemas as $schemas |
+([
+  path(.. | select(objects and has("resources")).resources | .[]) |
+  {"key": .[-1], "value": map(select(. != "resources"))}
+] | from_entries) as $libraryPath |
+[.. | select(objects and has("resources")).resources | map_values(.methods)] |
+add | (
+  keys |
+  map(
+    (split("_") | map((.[:1] | ascii_upcase) + .[1:]) | join(" ")) as $formattedName |
+    # {
+    #   "key": .,
+    #   "value": "Actions on \($formattedName)"
+    # } | from_entries) as $resourceDescriptions
+    {
+      "key": .,
+      "value": (
+        $formattedName |
+        gsub(" "; "")[:-1] |
+        if (in($schemas)) then . else $customSchemaMapping[.] end |
+        (. as $name | $schemas | .[$name]).properties |
+        map_values({type, help: .description}))}) | 
+  from_entries
+) as $bodyArgs | (
+  keys |
+  map(
+    (split("_") | map((.[:1] | ascii_upcase) + .[1:]) | join(" ")) as $formattedName |
+    {
+      "key": .,
+      "value": "Actions on \($formattedName)"
+    }) | 
+  from_entries
+) as $resourceDescriptions |
+to_entries | map({
+  name: (.["key"] | split("_") | map((.[:1] | ascii_upcase) + .[1:]) | join("") | (.[:1] | ascii_downcase) + .[1:]),
+  help: ($resourceDescriptions[.key]),
+  libraryPath: $libraryPath[.key],
+  endpoints: (
+    .key as $etype |
+    .value |
+    to_entries |
+    map(
+    .key as $method | 
+    .value | {
+      "name": $method,
+      "help": .description,
+      "pathVar": (if .parameters.path then "path" else (if .parameters.parent then "parent" else null end) end),
+      "args": (
+        (
+          .flatPath |
+          [scan("\\{([^}]+)\\}")[] | sub("sId$"; "Id")] |
+          map(
+            . as $argName |
+            $bodyArgs[$etype] |
+            (.[$argName] // {type: "string"}) as $argData |
+            {name: $argName, data: $argData}) 
+        ) + (
+          # For parameter in .parameterPath: $pdata
+          .parameters |
+          del(.path // .parent) |
+          to_entries |
+          map(
+            (if (.value.location == "query") then false else true end) as $isRequired | 
+            {
+              name: .key,
+              data: ( .value | { help: .description, type })
+            } |
+            .name |= (if ($isRequired | not) then "--" + . else . end))
+        ) + (
+          if .request then 
+            [{name: "body", data: {
+              help: "JSON file representing the \(.request["$ref"]) to \($method)",
+              "type": "FILE"
+            }}]
+          else null end)
+      )
+    })
+  )
+})
+
+# Current: $Resource.$Method
+# For each $Resource:
+#   - /// Add Description
+# For each $Resource.$Method:
+#   - /// Build help string
+#   - /// Parse Path / Parent for variables
+#   - /// Add all other parameters
+#   - /// Add "body" parameter if there's a request > "$ref" object
+#     - /// Fetch description from the linked schema
+# [scan("\\{([^}]+)\\}")[] | sub("sId$"; "Id")]
