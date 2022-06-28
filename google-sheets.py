b87dc6d699ef0d2f9b5addb1572c5295dfc03767
Steven Murray <44906675+StevenWMurray@users.noreply.github.com>
2022-06-28 14:55:57 -0500

    Add support for auth context switching
    This version of the Google Sheets CLI adds support for using a
    non-default authentication context. This feature adds a new CLI flag to
    enable specifying an alternate auth context. In addition, a few
    refactors occurred to support the slightly more complicated
    initialization flow.

--- google-sheets.py
+++ google-sheets.py
@@ -17,15 +17,8 @@ PathArgType = Optional[Literal["path", "parent"]]
 
 get_invoke = lambda obj, key: getattr(obj, key)()
 
-type_map = {
-    "string": str,
-    "integer": int,
-    "boolean": bool,
-    "FILE": argparse.FileType('r')
-}
-
-def parse_arg_data(data_path: Path) -> dict:
-    """Serialize arg data JSON into code
+def parse_discovery_data(data_path: Path) -> dict:
+    """Serialize discovery document data JSON into code
 
     In addition to loading the json, this substitutes a couple values in the
     arg data with functions.
@@ -33,15 +26,27 @@ def parse_arg_data(data_path: Path) -> dict:
     arg_data=json.loads(data_path.read_text())
     for entity in arg_data:
         for endpoint in entity["endpoints"]:
-            endpoint["library_func"] = getattr(
-                reduce(get_invoke, entity["libraryPath"], service),
+            endpoint['library_path'] = entity['libraryPath']
+            endpoint["library_func"] = lambda service, lib_path: getattr(
+                reduce(get_invoke, lib_path, service),
                 endpoint["name"])
             for arg in endpoint["args"]:
-                arg["data"]["type"] = type_map[arg["data"]["type"]]
-                if arg["name"] == "body":
-                    arg["data"]["default"] = sys.stdin
-        del entity["libraryPath"]
+                arg=parse_arg_data(arg)
     return arg_data
+
+
+def parse_arg_data(arg: dict) -> dict:
+    """Transform certain fields of the argument data"""
+    type_map = {
+        "string": str,
+        "integer": int,
+        "boolean": bool,
+        "FILE": argparse.FileType('r')
+    }
+    arg["data"]["type"] = type_map[arg["data"]["type"]]
+    if arg["name"] == "body":
+        arg["data"]["default"] = sys.stdin
+    return arg
 
 
 def add_entity_type_parser(parser, entity_type: dict) -> None:
@@ -59,18 +64,25 @@ def add_entity_type_parser(parser, entity_type: dict) -> None:
 def add_endpoint_parser(parser, endpoint: dict) -> None:
     """Simplified interface to add endpoint parsers to argparser"""
     endpoint_parser = parser.add_parser(endpoint['name'], help=endpoint['help'])
-    endpoint_parser.set_defaults(library_func=endpoint['library_func'])
+    endpoint_parser.set_defaults(
+        library_func=endpoint['library_func'],
+        library_path=endpoint['library_path'])
     for arg in endpoint['args']:
         endpoint_parser.add_argument(arg['name'], **arg.get('data', {}))
 
 
 def init_parsers(parser: argparse.ArgumentParser) -> None:
     """Add an argument parser for each supported entity type"""
+    parser.add_argument(
+        '--auth-context',
+        nargs='?',
+        default='GoogleAds',
+        help="Input which account the parser should authenticate against")
     subparser = parser.add_subparsers(
         description="Declare which entity type to operate on",
         required=True)
 
-    for entity_type in parse_arg_data(arg_data_path):
+    for entity_type in parse_discovery_data(arg_data_path):
         add_entity_type_parser(subparser, entity_type)
 
 
@@ -80,8 +92,12 @@ if __name__ == "__main__":
 
     init_parsers(parser)
     cmd_args = parser.parse_args()
-    library_function = cmd_args.library_func
+    context_key = cmd_args.auth_context
+    service = Services.from_auth_context(context_key).sheets_service
+    library_function = cmd_args.library_func(service, cmd_args.library_path)
     delattr(cmd_args, 'library_func')
+    delattr(cmd_args, 'library_path')
+    delattr(cmd_args, 'auth_context')
 
     if hasattr(cmd_args, 'body'):
         cmd_args.body = json.load(cmd_args.body)
