c454fb1a76b23fa8e59a29427b1fbc9277960835
Steven Murray <44906675+StevenWMurray@users.noreply.github.com>
2022-11-15 12:44:23 -0600

    Refactor UA reporting API CLI bindings
    This commit is primarily concerned with standardizing code styles in the
    UA reporting API CLI bindings script. In addition to this category of
    refactors, a new CLI option was added to treat API query input as either
    a pre-formed API request, or to use the new universal query format.
    Full support for the new query format is to be added in a later update.

--- ga-reporting.py
+++ ga-reporting.py
@@ -6,20 +6,25 @@ import json
 import argparse
 import sys
 import math
-import itertools
 import logging
 from typing import Iterator, NamedTuple
 from datetime import date, timedelta
 from copy import deepcopy
 from collections.abc import Mapping
+from itertools import chain
 
 from google_auth import Services, send_request
+
+import uar
+
 
 class DateRange(NamedTuple):
     start_date: str
     end_date: str
 
-service=Services.from_auth_context("GoogleAds").analytics_service
+
+service = Services.from_auth_context("GoogleAds").analytics_service
+
 
 def init_parsers(parser: argparse.ArgumentParser) -> None:
     """Spawns a parser to read the command line inputs
@@ -28,26 +33,36 @@ def init_parsers(parser: argparse.ArgumentParser) -> None:
     optional.
     """
     parser.add_argument(
-        'body',
-        nargs='?',
+        "body",
+        nargs="?",
         default=sys.stdin,
-        help='File containing the API request JSON',
-        type=argparse.FileType('r', encoding='UTF-8'))
+        help="File containing the API request(s)",
+        type=argparse.FileType("r", encoding="UTF-8"),
+    )
     parser.add_argument(
-        '--output-file',
-        '-o',
-        default=sys.stdout,
-        help='Output file to write the API response to',
-        type=argparse.FileType('w', encoding='UTF-8'))
-    parser.add_argument(
-        '--debug',
-        '-d',
+        "--use-new-query-format",
+        "-q",
         default=False,
-        help='Produce additional debugging output',
-        type=bool)
+        help="Declare whether to use the new universal query format, or parse input as a pre-built GA API request.",
+        type=bool,
+    )
+    parser.add_argument(
+        "--output-file",
+        "-o",
+        default=sys.stdout,
+        help="Output file to write the API response to",
+        type=argparse.FileType("w", encoding="UTF-8"),
+    )
+    parser.add_argument(
+        "--debug",
+        "-d",
+        default=False,
+        help="Produce additional debugging output",
+        type=bool,
+    )
 
 
-def read_input(cmd_args: argparse.Namespace) -> Iterator[dict]:
+def read_json_input(cmd_args: argparse.Namespace) -> Iterator[dict]:
     """Produce a stream of JSON report requests from CLI args
 
     This attempts to read the input file as a JSONL, or if that fails, then as a
@@ -57,13 +72,16 @@ def read_input(cmd_args: argparse.Namespace) -> Iterator[dict]:
     try:
         yield from (json.loads(line) for line in lines)
     except json.JSONDecodeError:
-        yield json.loads('\n'.join(lines))
+        yield json.loads("\n".join(lines))
 
 
 def has_sampling(response: dict) -> bool:
     """Check response for sampling"""
     for report in response["reports"]:
-        if "samplingSpaceSizes" in report["data"] or "samplesReadCounts" in report["data"]:
+        if (
+            "samplingSpaceSizes" in report["data"]
+            or "samplesReadCounts" in report["data"]
+        ):
             return True
     return False
 
@@ -74,58 +92,58 @@ def split_request(request_body: dict, response: dict):
     This takes into account the number of samples read vs. the sampling space,
     and divides the date range into even intervals.
     """
+
     def calculate_samples():
         """Guess # of intervals required to fix sampled request"""
-        num_sessions = int(response['reports'][0]['data']['samplingSpaceSizes'][0])
-        num_samples = int(response['reports'][0]['data']['samplesReadCounts'][0])
-        num_intervals = math.ceil(num_sessions / num_samples * 4/3)
+        num_sessions = int(response["reports"][0]["data"]["samplingSpaceSizes"][0])
+        num_samples = int(response["reports"][0]["data"]["samplesReadCounts"][0])
+        num_intervals = math.ceil(num_sessions / num_samples * 4 / 3)
         sampling = {
-            'sessions': num_sessions,
-            'samples': num_samples,
-            'num_intervals': num_intervals
+            "sessions": num_sessions,
+            "samples": num_samples,
+            "num_intervals": num_intervals,
         }
         logging.debug(f"ResponseSampling: {json.dumps(sampling)}")
         return sampling
 
     def generate_intervals(request_body: dict, num_intervals: int) -> list[list[dict]]:
         interval_groups = []
-        for date_range in request_body['reportRequests'][0]['dateRanges']:
+        for date_range in request_body["reportRequests"][0]["dateRanges"]:
             # Format: {startDate: str, endDate: str}
-            start = date.fromisoformat(date_range['startDate'])
-            end = date.fromisoformat(date_range['endDate'])
+            start = date.fromisoformat(date_range["startDate"])
+            end = date.fromisoformat(date_range["endDate"])
             temp = (end - start + timedelta(days=1)) / num_intervals
-            interval_len = timedelta(days=(temp.days+(temp.seconds>0)))
+            interval_len = timedelta(days=(temp.days + (temp.seconds > 0)))
 
             intervals = []
-            for i in range(0, num_intervals-1):
+            for i in range(0, num_intervals - 1):
                 int_end = start + interval_len - timedelta(days=1)
-                intervals.append({
-                    "startDate": start.isoformat(),
-                    "endDate": int_end.isoformat()
-                })
+                intervals.append(
+                    {"startDate": start.isoformat(), "endDate": int_end.isoformat()}
+                )
                 start += interval_len
-            intervals.append({
-                "startDate": start.isoformat(),
-                "endDate": end.isoformat()
-            })
+            intervals.append(
+                {"startDate": start.isoformat(), "endDate": end.isoformat()}
+            )
             interval_groups.append(intervals)
         return interval_groups
 
-    num_intervals = calculate_samples()['num_intervals']
+    num_intervals = calculate_samples()["num_intervals"]
     interval_groups = generate_intervals(request_body, num_intervals)
     new_date_ranges = zip(*interval_groups)
     for date_range_group in new_date_ranges:
         # copy required here to ensure that separate retry attempts don't
         # overwrite each other's date ranges
         rb = deepcopy(request_body)
-        for request in rb['reportRequests']:
-            request['dateRanges'] = list(date_range_group)
+        for request in rb["reportRequests"]:
+            request["dateRanges"] = list(date_range_group)
         yield rb
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description="Create requests against the GA Reporting API")
+        description="Create requests against the GA Reporting API"
+    )
 
     init_parsers(parser)
     cmd_args = parser.parse_args()
@@ -133,30 +151,31 @@ if __name__ == "__main__":
     if DEBUGGING:
         logging.basicConfig(level=logging.DEBUG)
 
-    queue = read_input(cmd_args)
+    queue = read_json_input(cmd_args)
     try:
         while True:
             request_body = next(queue)
             request = service.reports().batchGet(body=request_body)
             response = send_request(request)
 
-            if (has_sampling(response)):
+            if has_sampling(response):
                 bad_req = deepcopy(request_body)
-                logging.debug("SAMPLED RESPONSE: " + str(
-                    request_body['reportRequests'][0]['dateRanges']
-                ))
+                logging.debug(
+                    "SAMPLED RESPONSE: "
+                    + str(request_body["reportRequests"][0]["dateRanges"])
+                )
                 if DEBUGGING:
-                    print("ADD TO QUEUE" + str(
-                        request_body['reportRequests'][0]['dateRanges']
-                    ))
+                    print(
+                        "ADD TO QUEUE"
+                        + str(request_body["reportRequests"][0]["dateRanges"])
+                    )
                     bad_resp = deepcopy(response)
-                    bad_resp['request'] = bad_req
+                    bad_resp["request"] = bad_req
                     json.dump(bad_resp, cmd_args.output_file)
-                queue = itertools.chain(
-                    queue, split_request(bad_req, deepcopy(response)))
+                queue = chain(queue, split_request(bad_req, deepcopy(response)))
             else:
-                response['request'] = request_body
+                response["request"] = request_body
                 json.dump(response, cmd_args.output_file)
-                cmd_args.output_file.write('\n')
+                cmd_args.output_file.write("\n")
     except StopIteration:
         pass
